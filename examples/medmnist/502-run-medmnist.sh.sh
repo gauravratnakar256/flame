3b26306d719ba640c88da8e9a9686d2e0fd88ce9
Gaurav Gadewar <gauravgadewar35@hotmail.com>
2022-09-28 21:20:15 -0700

    Added runmedmnist.sh script

--- examples/medmnist/502-run-medmnist.sh.sh
+++ examples/medmnist/502-run-medmnist.sh.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]
+  then
+    echo "Provide correct number of arguments"
+	exit 1
+fi
+
+design=$1
+numberOfTrainer=$2
+
 export PATH="$HOME/.flame/bin:$PATH"
 
 flamectl create design medmnist -d "MedMNIST" --insecure
 flamectl create schema schema.json --design medmnist --insecure
-flamectl create code medmnist.zip --design medmnist --insecure
+flamectl create code $design.zip --design medmnist --insecure
 
 
-for i in {1..150}
+for i in {1..$numberOfTrainer}
 do
     flamectl create dataset dataset$i.json --insecure
 done
