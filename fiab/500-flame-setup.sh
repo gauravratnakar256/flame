f11270e9bd574c2716990e57ed38d4acf4428875
Gaurav Gadewar <gauravgadewar35@hotmail.com>
2022-09-28 22:23:23 -0700

    Updated readme

---
+++ fiab/500-flame-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh 
+
+# Install jq
+sudo apt update -y
+sudo apt-get install -y jq
+
+# Install Ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.3/deploy/static/provider/baremetal/deploy.yaml
+
+# Install Certificate Manager
+./setup-cert-manager.sh
+
+# install moreutils (sponge)
+sudo apt-get install moreutils
