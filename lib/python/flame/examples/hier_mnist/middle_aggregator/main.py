e3ab5eec56b75a50dee78eb162113debec535b71
gauravratnakar256 <gauravgadewar35@hotmail.com>
2022-12-01 14:45:55 -0800

    Removed wait join

--- lib/python/flame/examples/hier_mnist/middle_aggregator/main.py
+++ lib/python/flame/examples/hier_mnist/middle_aggregator/main.py
@@ -77,20 +77,7 @@ class PyTorchMnistMiddleAggregator(MiddleAggregator):
 
     def load_data(self) -> None:
         """Load a test dataset."""
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307, ), (0.3081, ))
-        ])
-
-        dataset = datasets.MNIST('./data',
-                                 train=False,
-                                 download=True,
-                                 transform=transform)
-
-        self.test_loader = torch.utils.data.DataLoader(dataset)
-
-        # store data into dataset for analysis (e.g., bias)
-        self.dataset = Dataset(dataloader=self.test_loader)
+        pass
 
     def train(self) -> None:
         """Train a model."""
@@ -99,33 +86,7 @@ class PyTorchMnistMiddleAggregator(MiddleAggregator):
 
     def evaluate(self) -> None:
         """Evaluate (test) a model."""
-        self.model.eval()
-        test_loss = 0
-        correct = 0
-        with torch.no_grad():
-            for data, target in self.test_loader:
-                data, target = data.to(self.device), target.to(self.device)
-                output = self.model(data)
-                test_loss += F.nll_loss(
-                    output, target,
-                    reduction='sum').item()  # sum up batch loss
-                pred = output.argmax(
-                    dim=1,
-                    keepdim=True)  # get the index of the max log-probability
-                correct += pred.eq(target.view_as(pred)).sum().item()
-        total = len(self.test_loader.dataset)
-        test_loss /= total
-        test_accuray = correct / total
-
-        logger.info(f"Test loss: {test_loss}")
-        logger.info(f"Test accuracy: {correct}/{total} ({test_accuray})")
-
-        # update metrics after each evaluation so that the metrics can be
-        # logged in a model registry.
-        self.update_metrics({
-            'test-loss': test_loss,
-            'test-accuracy': test_accuray
-        })
+        pass
 
 if __name__ == "__main__":
     import argparse
