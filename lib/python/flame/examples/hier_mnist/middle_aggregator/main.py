5233a424f49f0c1f1d0505c60e53f8c434594eff
gauravratnakar256 <93859207+gauravratnakar256@users.noreply.github.com>
2022-12-01 13:56:38 -0800

    Update main.py

--- lib/python/flame/examples/hier_mnist/middle_aggregator/main.py
+++ lib/python/flame/examples/hier_mnist/middle_aggregator/main.py
@@ -17,49 +17,44 @@
 
 import logging
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from flame.config import Config
-from flame.mode.horizontal.middle_aggregator import MiddleAggregator
-# the following needs to be imported to let the flame know
-# this aggregator works on tensorflow model
-from tensorflow import keras
+from flame.dataset import Dataset
+from flame.mode.horizontal.top_aggregator import TopAggregator
+from torchvision import datasets, transforms
 
 logger = logging.getLogger(__name__)
 
 
-class KerasMnistMiddleAggregator(MiddleAggregator):
-    """Keras Mnist Middle Level Aggregator."""
+class Net(nn.Module):
+    """Net class."""
 
-    def __init__(self, config: Config) -> None:
-        """Initialize a class instance."""
-        self.config = config
+    def __init__(self):
+        """Initialize."""
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-    def initialize(self):
-        """Initialize role."""
-        pass
-
-    def load_data(self) -> None:
-        """Load a test dataset."""
-        pass
-
-    def train(self) -> None:
-        """Train a model."""
-        pass
-
-    def evaluate(self) -> None:
-        """Evaluate (test) a model."""
-        pass
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('config', nargs='?', default="./config.json")
-
-    args = parser.parse_args()
-
-    config = Config(args.config)
-
-    a = KerasMnistMiddleAggregator(config)
-    a.compose()
-    a.run()
+    def forward(self, x):
+        """Forward."""
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+    
+    
