d6fec98e7984ab3c7becd99c4cc54beea3422beb
gauravratnakar256 <gauravgadewar35@hotmail.com>
2022-12-03 19:07:48 -0800

    Added resnet model

--- lib/python/flame/examples/hier_mnist/top_aggregator/main.py
+++ lib/python/flame/examples/hier_mnist/top_aggregator/main.py
@@ -23,6 +23,9 @@ from flame.dataset import Dataset # Not sure why we need this.
 from flame.mode.horizontal.top_aggregator import TopAggregator
 import torch
 import torchvision
+import medmnist
+from medmnist import INFO
+from sklearn.metrics import accuracy_score
 
 
 logger = logging.getLogger(__name__)
@@ -66,9 +69,41 @@ class PyTorchMedMNistAggregator(TopAggregator):
         self.model = CNN(num_classes=9)
 
     def load_data(self) -> None:
-        """Load a test dataset."""
-        # Implement this if loading data is needed in aggregator
-        pass
+        """MedMNIST Pathology Dataset
+        The dataset is kindly released by Jakob Nikolas Kather, Johannes Krisam, et al. (2019) in their paper 
+        "Predicting survival from colorectal cancer histology slides using deep learning: A retrospective multicenter study",
+        and made available through Yang et al. (2021) in 
+        "MedMNIST Classification Decathlon: A Lightweight AutoML Benchmark for Medical Image Analysis".
+        Dataset Repo: https://github.com/MedMNIST/MedMNIST
+        """
+        info = INFO["pathmnist"]
+        DataClass = getattr(medmnist, info['python_class'])
+ 
+        data_transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+        train_dataset = DataClass(split='train', transform=data_transform, download=True)
+        val_dataset = DataClass(split='val', transform=data_transform, download=True)
+
+        self.train_loader = torch.utils.data.DataLoader(
+            train_dataset, 
+            batch_size=self.batch_size, 
+            shuffle=True, 
+            num_workers=4 * torch.cuda.device_count(),
+            pin_memory=True,
+            drop_last=True
+        )
+        self.val_loader = torch.utils.data.DataLoader(
+            val_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=4 * torch.cuda.device_count(),
+            pin_memory=True,
+            drop_last=True
+        )
+
+        self.dataset_size = len(train_dataset)
 
     def train(self) -> None:
         """Train a model."""
@@ -76,9 +111,29 @@ class PyTorchMedMNistAggregator(TopAggregator):
         pass
 
     def evaluate(self) -> None:
-        """Evaluate (test) a model."""
-        # Implement this if testing is needed in aggregator
-        pass
+        """Evaluate a model."""
+        logger.info("Started Evaluating of Model")
+        self.model.eval()
+        loss_lst = list()
+        labels = torch.tensor([],device=self.device)
+        labels_pred = torch.tensor([],device=self.device)
+        with torch.no_grad():
+            for data, label in self.train_loader:
+                data, label = data.to(self.device), label.to(self.device)
+                output = self.model(data)
+                loss = self.criterion(output, label.squeeze())
+                loss_lst.append(loss.item())
+                labels_pred = torch.cat([labels_pred, output.argmax(dim=1)], dim=0)
+                labels = torch.cat([labels, label], dim=0)
+
+        labels_pred = labels_pred.cpu().detach().numpy()
+        labels = labels.cpu().detach().numpy()
+        val_acc = accuracy_score(labels, labels_pred)
+
+        val_loss = sum(loss_lst) / len(loss_lst)
+        self.update_metrics({"Val Loss": val_loss, "Val Accuracy": val_acc})
+        logger.info(f"Test Loss: {val_loss}")
+        logger.info(f"Test Accuracy: {val_acc}")
 
 if __name__ == "__main__":
     import argparse
