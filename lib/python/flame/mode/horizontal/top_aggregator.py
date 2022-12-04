03edf4b794a13bded0f28d732b83e81482a3d0cb
gauravratnakar256 <gauravgadewar35@hotmail.com>
2022-12-03 23:50:04 -0800

    Added resnet

--- lib/python/flame/mode/horizontal/top_aggregator.py
+++ lib/python/flame/mode/horizontal/top_aggregator.py
@@ -150,15 +150,15 @@ class TopAggregator(Role, metaclass=ABCMeta):
                 # save training result from trainer in a disk cache
                 self.cache[end] = tres
 
-        # optimizer conducts optimization (in this case, aggregation)
-        global_weights = self.optimizer.do(self.cache, total)
-        if global_weights is None:
-            logger.debug("failed model aggregation")
-            time.sleep(1)
-            return
+        # # optimizer conducts optimization (in this case, aggregation)
+        # global_weights = self.optimizer.do(self.cache, total)
+        # if global_weights is None:
+        #     logger.debug("failed model aggregation")
+        #     time.sleep(1)
+        #     return
 
-        # set global weights
-        self.weights = global_weights
+        # # set global weights
+        # self.weights = global_weights
 
         # update model with global weights
         self._update_model()
